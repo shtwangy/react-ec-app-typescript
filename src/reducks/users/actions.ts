12e02bd26f9baea483986cf1a10caaede1e4507f
shtwangy <shtwangy@gmail.com>
2020-12-17 11:55:57 +0900

    feat(src/reducks/users/actions.ts): create sign out action

--- src/reducks/users/actions.ts
+++ src/reducks/users/actions.ts
@@ -14,3 +14,15 @@ export const signInAction = (userState: UserState) => {
         }
     }
 }
+
+export const SIGN_OUT = 'SIGN_OUT'
+export const signOutAction = () => {
+    return {
+        type: SIGN_OUT,
+        payload: {
+            isSignedIn: false,
+            uid: '',
+            username: ''
+        }
+    }
+}
