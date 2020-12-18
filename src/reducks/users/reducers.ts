c71e46898c18740c5b5cf1728ff005f317b669a0
shtwangy <shtwangy@gmail.com>
2020-12-18 17:40:03 +0900

    feat(src/reducks/users/reducers.ts): add sign out action switch

--- src/reducks/users/reducers.ts
+++ src/reducks/users/reducers.ts
@@ -4,6 +4,7 @@ import initialState from "../store/initialState"
 export const UsersReducer = (state = initialState.users, action: Actions.UserAction) => {
     switch (action.type) {
         case Actions.SIGN_IN:
+        case Actions.SIGN_OUT:
             return {
                 ...state,
                 ...action.payload
