4133978450e1af66fe15abd2d5375851b1348590
shtwangy <shtwangy@gmail.com>
2020-12-18 17:39:18 +0900

    feat(src/reducks/store): create store file

---
+++ src/reducks/store/store.ts
@@ -0,0 +1,12 @@
+import {createStore as reduxCreateStore, combineReducers} from 'redux'
+import {UsersReducer} from '../users/reducers'
+
+const createStore = () => {
+    return reduxCreateStore(
+        combineReducers({
+            users: UsersReducer
+        })
+    )
+}
+
+export default createStore()
