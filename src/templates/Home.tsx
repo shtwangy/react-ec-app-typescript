f91492cf9b719ce61f111d061f8a83a3325cb3ba
shtwangy <shtwangy@gmail.com>
2020-12-19 12:06:34 +0900

    feat(src/templates/Home.tsx): create home component

---
+++ src/templates/Home.tsx
@@ -0,0 +1,14 @@
+import React, {FC} from 'react';
+
+interface Props {
+}
+
+const Home: FC<Props> = (props: Props) => {
+    return (
+        <div>
+            Home Template
+        </div>
+    );
+};
+
+export default Home;
