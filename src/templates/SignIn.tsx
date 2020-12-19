0eea4fc1dca79c78d2c31bccb5a5b39934d82eb4
shtwangy <shtwangy@gmail.com>
2020-12-19 12:06:54 +0900

    feat(src/templates/SignIn.tsx): create sign in component

---
+++ src/templates/SignIn.tsx
@@ -0,0 +1,14 @@
+import React, {FC} from 'react';
+
+interface Props {
+}
+
+const SignIn: FC<Props> = (props: Props) => {
+    return (
+        <div>
+            Sign In Template
+        </div>
+    );
+};
+
+export default SignIn;
