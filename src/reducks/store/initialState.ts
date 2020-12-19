1f0fd4611157b563873be805907396f0b771c289
shtwangy <shtwangy@gmail.com>
2020-12-19 12:35:02 +0900

    feat(src/reducks/store/initialState): add router to State interface

--- src/reducks/store/initialState.ts
+++ src/reducks/store/initialState.ts
@@ -1,6 +1,9 @@
 import {User} from '../../types/User'
+import {Reducer} from "redux";
+import {LocationChangeAction, RouterState} from "connected-react-router";
 
 export interface State {
+    router: Reducer<RouterState, LocationChangeAction> // TODO: ここに定義していいかは要検討
     users: User
 }
 
