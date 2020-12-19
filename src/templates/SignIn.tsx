0683f23c5c6dd4459d77f4cbd8be7d31c74d1d7d
shtwangy <shtwangy@gmail.com>
2020-12-19 12:35:55 +0900

    feat(src/templates/SignIn.tsx): display router value to console log

--- src/templates/SignIn.tsx
+++ src/templates/SignIn.tsx
@@ -1,9 +1,12 @@
 import React, {FC} from 'react';
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {push} from "connected-react-router";
+import {State} from "../reducks/store/initialState";
 
 const SignIn = () => {
     const dispatch = useDispatch();
+    const selector = useSelector((state: State) => state)
+    console.log(selector.router)
     return (
         <div>
             <h2>サインイン</h2>
