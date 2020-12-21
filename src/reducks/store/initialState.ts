070e2493bf6ff7b3760a1fc638be23d8ef129835
shtwangy <shtwangy@gmail.com>
2020-12-21 22:50:56 +0900

    refactor(src/reducks/store/initialState): change User to UserState

--- src/reducks/store/initialState.ts
+++ src/reducks/store/initialState.ts
@@ -1,10 +1,10 @@
-import {User} from '../../types/User'
+import {UserState} from '../users/types'
 import {Reducer} from "redux";
 import {LocationChangeAction, RouterState} from "connected-react-router";
 
 export interface State {
     router: Reducer<RouterState, LocationChangeAction> // TODO: ここに定義していいかは要検討
-    users: User
+    users: UserState
 }
 
 const initialState = {
