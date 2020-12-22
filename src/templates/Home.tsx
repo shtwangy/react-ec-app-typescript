abc62dc115e7d293e7f2b918bf0c234476120bc5
shtwangy <shtwangy@gmail.com>
2020-12-22 23:42:29 +0900

    feat(src/templates/Home.tsx): display username

--- src/templates/Home.tsx
+++ src/templates/Home.tsx
@@ -1,5 +1,6 @@
 import React, {FC} from 'react';
 import {getUserId} from "../reducks/users/selector";
+import {getUsername} from "../reducks/users/selector";
 import {State} from "../reducks/store/initialState";
 import {useSelector} from "react-redux";
 
@@ -9,10 +10,12 @@ interface Props {
 const Home: FC<Props> = (props: Props) => {
     const selector = useSelector((state: State) => state)
     const uid = getUserId(selector)
+    const username = getUsername(selector)
     return (
         <div>
             Home Template
-            <p>{uid}</p>
+            <p>UID: {uid}</p>
+            <p>ユーザー名: {username}</p>
         </div>
     );
 };
