e044fe184c756dcc3ccccecf0daba8a7b1f32e12
shtwangy <shtwangy@gmail.com>
2020-12-24 01:18:44 +0900

    feat(src/Router): change the component to be displayed in 'sign-in' routing to SignInContainer

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import {Route, Switch} from 'react-router'
 import {Home, SignIn} from './templates'
+import {SignInContainer} from "./containers";
 
 const Router =  () => {
     return (
         <Switch>
-            <Route exact path={'/sign-in'} component={SignIn} />
+            <Route exact path={'/sign-in'} component={SignInContainer} />
+            /* <Route exact path={'/sign-in'} component={SignIn} /> */
             <Route exact path={'(/)?'} component={Home} />
         </Switch>
     );
