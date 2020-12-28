795cc80c79e57eb43fe5ef13437c4464c3a56c07
shtwangy <shtwangy@gmail.com>
2020-12-28 14:29:09 +0900

    feat(src/Router): add sign up routing

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import {Route, Switch} from 'react-router'
-import {Home, SignIn} from './templates'
-import {SignInContainer} from "./containers";
+import {Home, SignIn, SignUp} from './templates'
 
 const Router =  () => {
     return (
         <Switch>
-            <Route exact path={'/sign-in'} component={SignInContainer} />
-            /* <Route exact path={'/sign-in'} component={SignIn} /> */
+            <Route exact path={'/sign-in'} component={SignIn} />
+            <Route exact path={'/sign-up'} component={SignUp} />
             <Route exact path={'(/)?'} component={Home} />
         </Switch>
     );
