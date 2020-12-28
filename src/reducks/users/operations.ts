301ed788d026d02eaef8b6d3a5667968398743df
shtwangy <shtwangy@gmail.com>
2020-12-28 22:36:04 +0900

    feat(src/reducks/users/operations): create sign-up operation

--- src/reducks/users/operations.ts
+++ src/reducks/users/operations.ts
@@ -2,6 +2,7 @@ import {signInAction} from "./actions";
 import {push} from 'connected-react-router'
 import {Dispatch} from "redux";
 import {State} from "../store/initialState";
+import {auth, db, FirebaseTimestamp} from '../../firebase'
 
 interface ApiResponse {
     login: string
@@ -30,5 +31,42 @@ export const signIn = () => {
 
             dispatch(push('/'))
         }
+    }
+}
+
+export const signUp = (username: string, email: string, password: string, confirmPassword: string) => {
+    return async (dispatch: Dispatch) => {
+        // validation check
+        if (username === '' && email === '' && password === '' && confirmPassword ==='') {
+            alert('必須項目が未入力です')
+            return false
+        }
+
+        if (password !== confirmPassword) {
+            alert('パスワードと確認用パスワードが一致しません')
+            return false
+        }
+        return auth.createUserWithEmailAndPassword(email, password)
+            .then(res => {
+                const user = res.user
+                if (user) {
+                    const uid = user.uid
+                    const timestamp = FirebaseTimestamp.now()
+
+                    const userInitialData = {
+                        created_at: timestamp,
+                        email: email,
+                        role: 'customer',
+                        uid: uid,
+                        updated_at: timestamp,
+                        username: username
+                    };
+
+                    db.collection('users').doc(uid).set(userInitialData)
+                        .then(() => {
+                            dispatch(push('/'))
+                        })
+                }
+            })
     }
 }
