541257720e414fe6b80199c8e1e30e441a62edd1
shtwangy <shtwangy@gmail.com>
2021-01-02 08:48:45 +0900

    feat(src/Auth): create Auth component

---
+++ src/Auth.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect } from "react";
+import {useDispatch, useSelector} from "react-redux";
+import { getIsSignedIn } from "./reducks/users/selectors";
+import { listenAuthState } from "./reducks/users/operations";
+import {State} from "./reducks/store/initialState";
+
+type Props = {
+    children: React.ReactElement;
+};
+
+const Auth: React.FC<Props> = (props: Props) => {
+    const dispatch = useDispatch();
+    const selector = useSelector((state: State) => state);
+    const isSignedIn = getIsSignedIn(selector);
+
+    const {children} = props
+    useEffect(() => {
+        if (!isSignedIn) {
+            dispatch(listenAuthState());
+        }
+    }, []);
+
+    if (!isSignedIn) {
+        return <></>
+    } else {
+        return children;
+    }
+};
+
+export default Auth;
