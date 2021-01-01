82a97b9dcf1236c4ebf6420623dd60b2af1e4c43
shtwangy <shtwangy@gmail.com>
2021-01-02 08:58:01 +0900

    chore(src/Auth): add console log for debugging around login

--- src/Auth.tsx
+++ src/Auth.tsx
@@ -12,10 +12,12 @@ const Auth: React.FC<Props> = (props: Props) => {
     const dispatch = useDispatch();
     const selector = useSelector((state: State) => state);
     const isSignedIn = getIsSignedIn(selector);
+    console.log(isSignedIn)
 
     const {children} = props
     useEffect(() => {
         if (!isSignedIn) {
+            console.log('run listenAuthState')
             dispatch(listenAuthState());
         }
     }, []);
