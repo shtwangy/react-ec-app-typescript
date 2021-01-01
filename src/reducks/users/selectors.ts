f33b4fab2f5f4b7e07a32e55d3b1babaa3c1e2e5
shtwangy <shtwangy@gmail.com>
2021-01-02 08:48:17 +0900

    feat(src/reducks/users/selectors): rename and add get isSignedIn selector

--- src/reducks/users/selectors.ts
+++ src/reducks/users/selectors.ts
@@ -3,6 +3,11 @@ import {State} from "../store/initialState";
 
 const usersSelector = (state: State) => state.users;
 
+export const  getIsSignedIn = createSelector(
+    [usersSelector],
+    state => state.isSignedIn
+)
+
 export const  getUserId = createSelector(
     [usersSelector],
     state => state.uid
