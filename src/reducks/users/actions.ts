ed730c12170b915d4b8d8a52da559963dbb7a7b6
shtwangy <shtwangy@gmail.com>
2021-01-01 16:51:04 +0900

    fix(src/reducks/users/actions.ts): correction by UserState interface modify

--- src/reducks/users/actions.ts
+++ src/reducks/users/actions.ts
@@ -1,16 +1,17 @@
-import {UserState} from "./types";
+import {User} from "./types";
 
 export interface UserAction {
     type: string
-    payload: UserState
+    payload: User
 }
 
 export const SIGN_IN = 'SIGN_IN'
-export const signInAction = (userState: UserState): UserAction => {
+export const signInAction = (userState: User): UserAction => {
     return {
         type: SIGN_IN,
         payload: {
             isSignedIn: true,
+            role: userState.role,
             uid: userState.uid,
             username: userState.username
         }
@@ -23,6 +24,7 @@ export const signOutAction = (): UserAction => {
         type: SIGN_OUT,
         payload: {
             isSignedIn: false,
+            role: '',
             uid: '',
             username: ''
         }
