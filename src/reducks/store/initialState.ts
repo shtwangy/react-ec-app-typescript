82af6acfe7895e4b4dce1c51adf664ee3d92076f
shtwangy <shtwangy@gmail.com>
2021-01-01 16:49:00 +0900

    fix(src/reducks/store/initialState): correction by renaming

--- src/reducks/store/initialState.ts
+++ src/reducks/store/initialState.ts
@@ -1,10 +1,10 @@
-import {UserState} from '../users/types'
+import {User} from '../users/types'
 import {Reducer} from "redux";
 import {LocationChangeAction, RouterState} from "connected-react-router";
 
 export interface State {
     router: Reducer<RouterState, LocationChangeAction> // TODO: ここに定義していいかは要検討
-    users: UserState
+    users: User
 }
 
 const initialState = {
