7bba147fdee9d5442d4c16252709e84f401d88de
shtwangy <shtwangy@gmail.com>
2021-01-04 01:09:07 +0900

    feat(src/Router): add password reset routing

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import {Route, Switch} from 'react-router'
 import Auth from './Auth'
-import {Home, SignIn, SignUp} from './templates'
+import {Home, PasswordReset, SignIn, SignUp} from './templates'
 
 const Router =  () => {
     return (
         <Switch>
             <Route exact path={'/sign-in'} component={SignIn} />
+            <Route exact path={'/sign-in/reset'} component={PasswordReset} />
             <Route exact path={'/sign-up'} component={SignUp} />
             <Auth>
                 <Route exact path={'(/)?'} component={Home} />
