b814b69964a04831ad23ec84c73261ff231dfe3c
shtwangy <shtwangy@gmail.com>
2021-01-04 01:07:13 +0900

    feat(src/reducks/users/operations): add reset password operation

--- src/reducks/users/operations.ts
+++ src/reducks/users/operations.ts
@@ -27,6 +27,23 @@ export const listenAuthState = () => {
     }
 }
 
+export const resetPassword = (email: string) => {
+    return async (dispatch: Dispatch) => {
+        // validation TODO: メソッド化する
+        if (email === '') {
+            alert('必須項目が未入力です');
+            return false;
+        } else {
+            auth.sendPasswordResetEmail(email).then(() => {
+                alert('入力されたアドレスにパスワードリセット用のメールを送りました。');
+                dispatch(push('/'));
+            }).catch(() => {
+                alert('パスワードリセットに失敗しました。');
+            });
+        }
+    }
+};
+
 export const signIn = (email: string, password: string) => {
     return async (dispatch: Dispatch) => {
         // validation check
